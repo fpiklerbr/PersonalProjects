05ec27fa098f4db21fb08fa450fdc30ce301514f
fpiklerbr <fernandopikler@gmail.com>
2023-12-25 19:34:58 -0300

    included google auth

--- _etl_scrips/_import_data_functions.py
+++ _etl_scrips/_import_data_functions.py
@@ -1,1 +1,28 @@
-## THIS FILE IS MEANT TO CONSOLIDATE FUNCTIONS WHICH WILL BE USED TO CREATE IMPORT SCRIPTS AND APIS WITH A WIDE RANGE OF SYSTEMS INTO ANY WAREHOUSE
+import boto3
+from botocore.exceptions import ClientError
+
+def initialize_aws_client():
+    """
+    Initialize AWS client for Systems Manager.
+    """
+    try:
+        client = boto3.client('ssm')
+        return client
+    except ClientError as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def get_parameter(client, parameter_name, with_decryption=True):
+    """
+    Fetch a parameter from AWS Systems Manager Parameter Store.
+    :param client: Boto3 client
+    :param parameter_name: Name of the parameter
+    :param with_decryption: Boolean to determine if the parameter is encrypted
+    :return: Parameter value or None if not found or on error
+    """
+    try:
+        response = client.get_parameter(Name=parameter_name, WithDecryption=with_decryption)
+        return response['Parameter']['Value']
+    except ClientError as e:
+        print(f"Could not fetch parameter: {e}")
+        return None
